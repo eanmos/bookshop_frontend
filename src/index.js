a576dc2059e768ed00b1e7f847862c63376f6ea0
Nefedov Danil <eanmos@ya.ru>
2021-12-15 06:28:41 +0100

    fix: removed empty components to prevent warnings

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ import SignInPage from "components/SignInPage"
 import SignUpPage from "components/SignUpPage"
 import CartPage from "components/CartPage"
 import BookPage from "components/BookPage"
-import AuthorPage from "components/AuthorPage"
-import AddBookPage from "components/AddBookPage"
-import AddAuthorPage from "components/AddAuthorPage"
 
 import { __FAKE_DATA__ } from "./__fake_data__"
 import "./index.css"
@@ -26,9 +23,6 @@ ReactDOM.render(
         <Route path="/signUp" element={ <SignUpPage /> } />
         <Route path="/cart" element={ <CartPage books={ __FAKE_DATA__.books } /> } />
         <Route path="/books/:bookId" element={ <BookPage books={ __FAKE_DATA__.books } /> } />
-        <Route path="/authors/:authorId" element={ <AuthorPage /> } />
-        <Route path="/addBook" element={ <AddBookPage /> } />
-        <Route path="/addAuthor" element={ <AddAuthorPage /> } />
       </Routes>
     </BrowserRouter>
   </>,

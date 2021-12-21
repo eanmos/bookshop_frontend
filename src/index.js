b855b7eccd151c82a6ee5ed7c6d086fd5c06df38
Nefedov Danil <eanmos@ya.ru>
2021-12-21 19:19:38 +0300

    feat: fetch books from server

--- src/index.js
+++ src/index.js
@@ -10,21 +10,29 @@ import SignUpPage from "components/SignUpPage"
 import CartPage from "components/CartPage"
 import BookPage from "components/BookPage"
 
-import { __FAKE_DATA__ } from "./__fake_data__"
 import "./index.css"
 
-ReactDOM.render(
-  <>
-    <ToastContainer />
-    <BrowserRouter>
-      <Routes>
-        <Route exact index path="/" element={ <HomePage books={ __FAKE_DATA__.books } /> } />
-        <Route path="/signIn" element={ <SignInPage /> } />
-        <Route path="/signUp" element={ <SignUpPage /> } />
-        <Route path="/cart" element={ <CartPage books={ __FAKE_DATA__.books } /> } />
-        <Route path="/books/:bookId" element={ <BookPage books={ __FAKE_DATA__.books } /> } />
-      </Routes>
-    </BrowserRouter>
-  </>,
-  document.getElementById('root')
-)
+(async () => {
+	const getBooks = async () => {
+		const response = await fetch("/getBooks")
+		return await response.json()
+	}
+
+	const books = await getBooks()
+
+	ReactDOM.render(
+	  <>
+	    <ToastContainer />
+	    <BrowserRouter>
+	      <Routes>
+		<Route exact index path="/" element={ <HomePage books={books} /> } />
+		<Route path="/signIn" element={ <SignInPage /> } />
+		<Route path="/signUp" element={ <SignUpPage /> } />
+		<Route path="/cart" element={ <CartPage books={books} /> } />
+		<Route path="/books/:bookId" element={ <BookPage books={books} /> } />
+	      </Routes>
+	    </BrowserRouter>
+	  </>,
+	  document.getElementById('root')
+	)
+})()
